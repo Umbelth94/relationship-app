fd07767bd9776b93168e6fbf849e58bcc634b0cc
Travis Umbel <Umbelth94@gmail.com>
2024-07-15 18:39:35 -0500

    button routes to api/login and api/logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/navigation";
+
 
 export default function Home() {
   let { user, error, isLoading } = useUser();
+
+  const router = useRouter()  
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -11,12 +15,12 @@ export default function Home() {
     <main className="flex w-screen min-h-screen">
       <div className="flex w-screen h-[2rem] bg-slate-50">
         {!user && (
-          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white ">
+          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white " onClick={() => router.push('/api/auth/login')}>
             Sign In
           </button>
         )}
         {user && (
-          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white ">
+          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white " onClick={() => router.push('/api/auth/logout')}>
             Sign Out
           </button>
         )}
