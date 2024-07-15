779790eff1b67e257bb996a3031edccadd5ba56f
mike <95.michael.schroeder@gmail.com>
2024-07-15 18:13:54 -0500

    formatting

--- src/__tests__/components/layout.test.tsx
+++ src/__tests__/components/layout.test.tsx
@@ -2,13 +2,15 @@ import RootLayout from "@/app/layout";
 import { render } from "@testing-library/react";
 
 describe("Layout", () => {
-
-  it("should set web page title to Ourchive", () => {
-    let { container } = render(<RootLayout><div></div></RootLayout>);
+  it("should set web page title to Ourkive", () => {
+    let { container } = render(
+      <RootLayout>
+        <div></div>
+      </RootLayout>,
+    );
 
     let title = container.querySelector("title");
-    
+
     expect(title?.textContent).toBe("Ourkive");
   });
-
 });
