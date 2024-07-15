7edd82989b3d2572b7e90bfb442bd59363f9d760
mike <95.michael.schroeder@gmail.com>
2024-07-15 18:04:50 -0500

    fix test

--- src/__tests__/components/layout.test.tsx
+++ src/__tests__/components/layout.test.tsx
@@ -2,9 +2,13 @@ import RootLayout from "@/app/layout";
 import { render } from "@testing-library/react";
 
 describe("Layout", () => {
+
   it("should set web page title to Ourchive", () => {
-    let { container } = render(<RootLayout children={undefined} />);
+    let { container } = render(<RootLayout><div></div></RootLayout>);
+
     let title = container.querySelector("title");
+    
     expect(title?.textContent).toBe("Ourkive");
   });
+
 });
