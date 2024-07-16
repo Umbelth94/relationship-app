fe3696525f9971586e337f96a31c878fd8aee03f
Melissa <msmelissacz21@gmail.com>
2024-07-15 19:27:08 -0500

    added favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,10 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { useRouter } from "next/navigation";
 
-
 export default function Home() {
   let { user, error, isLoading } = useUser();
 
-  const router = useRouter()  
+  const router = useRouter();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -15,12 +14,18 @@ export default function Home() {
     <main className="flex w-screen min-h-screen">
       <div className="flex w-screen h-[2rem] bg-slate-50">
         {!user && (
-          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white " onClick={() => router.push('/api/auth/login')}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white "
+            onClick={() => router.push("/api/auth/login")}
+          >
             Sign In
           </button>
         )}
         {user && (
-          <button className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white " onClick={() => router.push('/api/auth/logout')}>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 rounded p-[.5rem] text-white "
+            onClick={() => router.push("/api/auth/logout")}
+          >
             Sign Out
           </button>
         )}
