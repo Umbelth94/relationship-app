aba780ee297d829d2d67853a4f39d0b12d4d1645
Melissa <msmelissacz21@gmail.com>
2024-07-22 19:52:38 -0500

    Continuing to get fetching for scrapbooks to do things

--- src/app/hooks/photos/useScrapbooks.tsx
+++ src/app/hooks/photos/useScrapbooks.tsx
@@ -1,15 +1,16 @@
 import Scrapbook from "@/app/models/scrapbook/scrapbook";
 import ScrapbookCollectionResponseData from "@/app/models/scrapbook/scrapbooksCollectionResponseData";
 import { useUser } from "@auth0/nextjs-auth0/client";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 
-export function useScrapbooks():Scrapbook[] {
+export function useScrapbooks(): Scrapbook[] {
   const { user } = useUser();
-  const [scrapbooks, setScrapbooks]= useState<Scrapbook[]>([]);
-  let request = fetch(`${process.env.API_BASE_URL}api/protected/scrapbooks`);
+  const [scrapbooks, setScrapbooks] = useState<Scrapbook[]>([]);
+  const [ didFetch, setDidFetch ] = useState<boolean>(false)
   useEffect(() => {
-    if (user) {
-      request.then((res) => {
+    if (user && didFetch == false) {
+      setDidFetch(true)
+      fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}api/protected/scrapbooks`).then((res) => {
         res.json().then((data: ScrapbookCollectionResponseData) => {
           setScrapbooks(data.scrapbooks);
         });
--- src/app/scrapbook/page.tsx
+++ src/app/scrapbook/page.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import { useScrapbooks } from "../hooks/photos/useScrapbooks";
 
 export default function Scrapbook() {
-  const scrapbooks = useScrapbooks()
+  const scrapbooks = useScrapbooks();
   return (
     <main className="h-screen w-screen">
       <div id="scrapbook-select" className="">
-        {scrapbooks.map(scrapbook=>{
-          return<div>{scrapbook.title}</div>
+        {scrapbooks.map((scrapbook) => {
+          return <div>{scrapbook.title}</div>;
         })}
       </div>
     </main>
