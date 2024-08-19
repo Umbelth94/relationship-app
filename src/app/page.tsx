a6ddecfd831f1c5b4ddbddcde9316954459ffbb1
mike <95.michael.schroeder@gmail.com>
2024-08-19 11:53:18 -0500

    add component nest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 export default function Home() {
   let { user, error, isLoading } = useUser();
-
   return (
     <main className="flex-column w-screen min-h-screen">
       {user && <h1>Hello {user.name}</h1>}
