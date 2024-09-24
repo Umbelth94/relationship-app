2a8884eca1480923a7f960a84fa4d3693e9bc5dc
Travis Umbel <Umbelth94@gmail.com>
2024-09-23 19:50:29 -0500

    fixed build

--- src/app/api/protected/status/route.ts
+++ src/app/api/protected/status/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const user = await getSession();
-  console.log(user.user.sub);
+  console.log(user?.user.sub);
   console.log("blah");
   return NextResponse.json({ message: "You are authorized bitch" });
 }
