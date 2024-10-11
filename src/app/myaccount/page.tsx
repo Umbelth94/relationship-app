0af67b7499ebbdf4a8d60bd83ee80832c208a14a
Travis Umbel <Umbelth94@gmail.com>
2024-10-11 14:50:17 -0500

    switch names to camelcase

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -34,7 +34,7 @@ const MyAccount: NextPage = withPageAuthRequired(
           <input type="text" name="pronouns" id="pronouns" />
           <br></br>
           <label>About Me</label>
-          <textarea name="aboutme" id="aboutme"></textarea>
+          <textarea name="aboutMe" id="aboutMe"></textarea>
           <br></br>
           <label>Adress Line 1:</label>
           <input type="text" name="line1" id="line1" />
@@ -60,7 +60,7 @@ const MyAccount: NextPage = withPageAuthRequired(
           <input type="tel" name="phone" id="phone" />
           <br></br>
           <label>Birthday</label>
-          <input type="date" name="birthdate" id="birthdate" />
+          <input type="date" name="birthDate" id="birthDate" />
           <br></br>
           <h1>Preferences and Interests</h1>
           {/* TODO: Probably make these checkboxes with as many options as we can think of?  */}
@@ -68,10 +68,10 @@ const MyAccount: NextPage = withPageAuthRequired(
             Favorite Date Activities(e.g., dining out, outdoor adventures, movie
             nights)
           </label>
-          <textarea name="date-activities" id="date-activities"></textarea>
+          <textarea name="dateActivities" id="dateActivities"></textarea>
           <br></br>
           <label>Preferred Date Times (e.g. weekends, evenings)</label>
-          <textarea name="date-times" id="date-times"></textarea>
+          <textarea name="dateTimes" id="dateTimes"></textarea>
           <br></br>
           <label>Hobbies & Interests (e.g., reading, hiking, cooking)</label>
           <textarea name="hobbies" id="hobbies"></textarea>
