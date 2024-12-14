a71a0841d726d8b51a8ac3666cc2fa45ed015e02
Travis Umbel <Umbelth94@gmail.com>
2024-12-14 16:18:04 -0600

    Finished first pass of the form

--- src/app/myaccount/page.tsx
+++ src/app/myaccount/page.tsx
@@ -1,10 +1,33 @@
-//TODO: Update UserProfile state after saving/updating profile into the database.
+//TODO: Finish Basic Form Layout and Inputs
+//TODO: Make Sure form saves data to database
+//TODO: Validation, Proper Form Inputs
+//TODO: Make it look *clicks tongue* REAAALL nice, clark.
 "use client";
 import { NextPage } from "next";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { Dispatch, SetStateAction, useContext, useState } from "react";
 import { UserProfileContext } from "../provider/userProfileProvider";
 import { createUserFormDataObject, UserProfile } from "../models/UserProfile";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type UserProfileFields = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  pronouns: string;
+  aboutMe: string;
+  line1: string;
+  line2: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+  phone: string;
+  birthDate: string;
+  dateActivities: string;
+  dateTimes: string;
+  hobbies: string;
+};
 
 //Auth0 middleware that checks if the user is authenticated
 const MyAccount: NextPage = withPageAuthRequired(
@@ -12,8 +35,141 @@ const MyAccount: NextPage = withPageAuthRequired(
     const { userProfile, setUserProfile } = useContext(UserProfileContext);
     let userInfo = userProfile;
 
+    const {
+      register,
+      handleSubmit,
+      watch,
+      formState: { errors },
+    } = useForm<UserProfileFields>();
+    const onSubmit: SubmitHandler<UserProfileFields> = (data) =>
+      console.log(data);
+
+    // watch is used for doing something with the value of a field, like conditional rendering
+    //console.log(watch("example"))
+
     return (
-      <main className="h-screen w-screen dark:text-dark-font-color-1">
+      <main className="h-screen w-screen">
+        <div className="w-[50rem] h-fit bg-primary dark:bg-dark-1">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex justify-between p-6">
+              <div>
+                <h2>Personal Info</h2>
+              </div>
+              <div className="grid-cols-1 grid gap-y-[2rem]">
+                <div>
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    placeholder="First Name"
+                    {...register("firstName", { required: true })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    placeholder="Last Name"
+                    {...register("lastName", { required: true })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="birthDate">Birth Date</label>
+                  <input placeholder="12/02/1994" {...register("birthDate")} />
+                </div>
+              </div>
+            </div>
+            <hr />
+
+            <div className="flex justify-between p-6">
+              <div>
+                <h2>Contact Info</h2>
+              </div>
+              <div className="grid-cols-1 grid gap-y-[2rem]">
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    placeholder="JaneAndJohn@doe.com"
+                    {...register("email", { required: true })}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="phone">Phone #</label>
+                  <input placeholder="555-555-5555" {...register("phone")} />
+                </div>
+              </div>
+            </div>
+            <hr />
+
+            <div className="flex justify-between p-6">
+              <div>
+                <h2>Address</h2>
+              </div>
+              <div className="grid-cols-1 grid gap-y-[2rem]">
+                <div>
+                  <label htmlFor="line1">Street</label>
+                  <input
+                    placeholder="1234 Streetname Lane"
+                    {...register("line1")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="line2">Apartment Number</label>
+                  <input placeholder="Apartment 1" {...register("line2")} />
+                </div>
+                <div className="flex flex-row space-between">
+                  <div>
+                    <label htmlFor="city">City</label>
+                    <input placeholder="Madison" {...register("city")} />
+                  </div>
+                  <div>
+                    <label htmlFor="state">State</label>
+                    <input placeholder="Wisconsin" {...register("state")} />
+                  </div>
+                  <div>
+                    <label htmlFor="country">Country</label>
+                    <input
+                      placeholder="United States"
+                      {...register("country")}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+
+            <div className="flex justify-between p-6">
+              <div>
+                <h2>Interests</h2>
+              </div>
+              <div className="grid-cols-1 grid gap-y-[2rem]">
+                <div>
+                  <label htmlFor="dateActivities">
+                    Favorite Date Activities
+                  </label>
+                  <input
+                    placeholder="Long walks on the beach..."
+                    {...register("dateActivities")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="dateTimes">Favorite Time For a Date</label>
+                  <input
+                    placeholder="Evening, Afternoon"
+                    {...register("dateTimes")}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="hobbies">Favorite Hobbies</label>
+                  <input
+                    placeholder="Beach Walking, Warhammer Figure Painting"
+                    {...register("hobbies")}
+                  />
+                </div>
+              </div>
+            </div>
+            <input type="submit" />
+          </form>
+        </div>
+
+        {/* previous form */}
         <form id="profile">
           <h1>Personal Information</h1>
           <label>First Name</label>
