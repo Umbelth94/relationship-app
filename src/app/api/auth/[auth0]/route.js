39b02b3c4587fb7a7afae1f65ef50e0393dd6044
Travis Umbel <Umbelth94@gmail.com>
2025-01-15 18:15:33 -0600

    added notes detailing certain API auth functions that arent very clear from just the code

--- src/app/api/auth/[auth0]/route.js
+++ src/app/api/auth/[auth0]/route.js
@@ -1,3 +1,6 @@
+//Creates the auth handler routes that are used throughout the application
+//login, callback, logout, and me
+
 import { handleAuth } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth();
