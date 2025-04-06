23ccf644a504c6151a796a276fdfc4e0772ff339
Travis Umbel <Umbelth94@gmail.com>
2025-04-06 13:35:10 -0500

    fixed input on my-dates page

--- src/app/my-dates/my-dates.tsx
+++ src/app/my-dates/my-dates.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0/client";
 import { NextPage } from "next";
 
 const MyDates: NextPage = withPageAuthRequired(
-  async () => {
+  () => {
     return (
       <main>
         <div>
