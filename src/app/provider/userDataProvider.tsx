28e78d4d182bea58416c9ca67d85359f295b091a
Travis Umbel <Umbelth94@gmail.com>
2025-04-09 17:47:45 -0500

    Add useEffect for updating dates state variable

--- src/app/provider/userDataProvider.tsx
+++ src/app/provider/userDataProvider.tsx
@@ -13,17 +13,14 @@ import {
 import { UserProfile } from "../models/UserProfile";
 import { UserDate } from "../api/protected/dates/route";
 
-interface UserData {
-  userProfile: UserProfile;
-  userDates: UserDate[];
-}
-
-interface UserProfileContextProps {
-  userData?: UserData;
+interface UserDataContextProps {
+  userDates?: UserDate[];
+  setUserDates?: Dispatch<SetStateAction<UserDate[] | undefined>>;
+  userProfile?: UserProfile;
   setUserProfile?: Dispatch<SetStateAction<UserProfile | undefined>>;
 }
 
-export const UserProfileContext = createContext<UserProfileContextProps>({});
+export const UserProfileContext = createContext<UserDataContextProps>({});
 
 export default function UserProfileProvider({
   children,
@@ -31,19 +28,20 @@ export default function UserProfileProvider({
   children: React.ReactNode;
 }>) {
   const [userProfile, setUserProfile] = useState<UserProfile>();
+  const [userDates, setUserDates] = useState<UserDate[]>();
 
   // Run a fetch request to grab the user's profile information
   useEffect(() => {
-    console.log("Provider useEffect initialized");
+    console.log("userProfile provider useEffect initialized");
     if (typeof window !== "undefined") {
       console.log("fetching profile data");
       fetch(`${window.location.origin}/api/protected/user/`).then((data) => {
         //Set userProfile state variable
         if (data.status === 200) {
-          data.json().then((userData) => {
+          data.json().then((profileData) => {
             // debugger;
-            if (JSON.stringify(userData) !== JSON.stringify(userProfile)) {
-              setUserProfile(userData);
+            if (JSON.stringify(profileData) !== JSON.stringify(userProfile)) {
+              setUserProfile(profileData);
             } else {
               return;
             }
@@ -53,9 +51,29 @@ export default function UserProfileProvider({
     }
   }, [userProfile]);
 
+  useEffect(() => {
+    console.log("userDates provider useEffect initialized");
+    if (typeof window !== "undefined") {
+      console.log("fetching date data");
+      fetch(`${window.location.origin}/api/protected/dates`).then((dates) => {
+        if (dates.status === 200) {
+          dates.json().then((dateData) => {
+            if (JSON.stringify(dateData) !== JSON.stringify(userDates)) {
+              setUserDates(dateData);
+            } else {
+              return;
+            }
+          });
+        }
+      });
+    }
+  }, [userDates]);
+
   return (
     //Wrap the child elements inside of the provider
-    <UserProfileContext.Provider value={{ userProfile, setUserProfile }}>
+    <UserProfileContext.Provider
+      value={{ userProfile, setUserProfile, userDates, setUserDates }}
+    >
       {children}
     </UserProfileContext.Provider>
   );
